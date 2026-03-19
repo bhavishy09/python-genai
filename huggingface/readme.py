02d82324da2ca613625642d6a096633942063cb2
bhavishy09 <futuresingh111@gmail.com>
2026-03-20 01:14:53 +0530

    new

---
+++ huggingface/readme.py
@@ -0,0 +1,32 @@
+"""just like github for LLM models 
+
+hugging face is a platform for sharing and discovering machine learning models,
+particularly those related to natural language processing (NLP).
+It provides a repository of pre-trained models, 
+tools for fine-tuning and deploying models,
+ and a community for collaboration and knowledge sharing.
+ Hugging Face is known for its Transformers library, 
+ which offers a wide range of pre-trained models for various NLP tasks such as text classification, 
+question answering, and language generation. The platform also supports other types of models beyond NLP, making it a versatile resource for machine learning practitioners.
+
+
+-> go on hugging face website and choose any model and go to use model and transformers
+
+"""
+
+
+"""
+
+hugging face cli-> pip install "huggingface_hub[cli]"
+brew install huggingface-cli
+
+login -> huggingface-cli login
+ -> Enter your token (input will not be visible): 
+ so how to generate a token ? go in hyggung face settings create new token and copy it and paste here
+"""
+
+
+"""
+transformers -> pip install transformers
+
+"""
